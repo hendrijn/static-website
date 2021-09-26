02b5d9b5995c2ac50e72ba66f1b09eeb255f45f4
Jacquelyn Hendricks <hendrijn@dukes.jmu.edu>
2021-09-26 14:51:37 -0400

    Started responsive design.

---
+++ javascript/main.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector(".burger");
+const navlinks = document.querySelector(".navlinks");
+
+burger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    burger.classList.toggle("active");
+    navlinks.classList.toggle("active");
+}
